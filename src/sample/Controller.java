da421ef9d3a6071931307dc3edcf6ea4f28bbe2d
andriybobchuk <andriybobchuk@gmail.com>
2020-09-04 19:47:09 +0300

    Added splash screen on app start (create new/Open existing)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,27 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.print.JobSettings;
+import javafx.print.PrinterJob;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+
 import java.io.*;
 
-public class Controller {
+public class Controller<font> {
 
     @FXML
     private Button btn_Write;
@@ -29,6 +39,90 @@ public class Controller {
     private HBox hb_MistakePan;
     @FXML
     private HBox hb_FinishPan;
+    @FXML
+    private ComboBox cb_Font;
+    @FXML
+    private Button btn_Print;
+    @FXML
+    private HBox hb_Menu;
+    @FXML
+    private Pane p_Hello;
+
+     
+
+//    public void getTa_Text() {
+//        Text text = new Text(ta_TextArea.getText());
+//        Font font = Font.loadFont("file:resources/fonts/NinaCTT.ttf", 45);
+//        text.setFont(font);
+//    }
+
+//    static ObservableList<String> jvmChoices = FXCollections.observableArrayList(
+//            "Java",
+//            "Kotlin",
+//            "Groovy",
+//            "Scala",
+//            "Clojure"
+//    );
+//
+//    public void getCb_Font(ActionEvent e) {
+//        cb_Font.setItems(jvmChoices);
+//
+//    }
+
+    @FXML
+    public void onActionPageSetup(ActionEvent e) {
+        PrinterJob printerJob = createPrinterJob();
+        printerJob.showPageSetupDialog(ta_TextArea
+                .getScene()
+                .getWindow());
+        JobSettings jobSettings = printerJob.getJobSettings();
+    }
+
+    private PrinterJob createPrinterJob() {
+        PrinterJob printerJob = PrinterJob
+                .createPrinterJob();
+        JobSettings jobSettings = null;
+        if (jobSettings == null) {
+            jobSettings = printerJob.getJobSettings();
+        } else {
+            setPrinterJobSettings(printerJob
+                            .getJobSettings(),
+                    jobSettings);
+        }
+        return printerJob;
+    }
+
+    public void setPrinterJobSettings(JobSettings from, JobSettings to) {
+        to.setCollation(from.getCollation());
+        to.setCopies(from.getCopies());
+        to.setJobName(from.getJobName());
+        to.setPageLayout(from.getPageLayout());
+        to.setPageRanges(from.getPageRanges());
+        to.setPaperSource(from.getPaperSource());
+        to.setPrintColor(from.getPrintColor());
+        to.setPrintQuality(from.getPrintQuality());
+        to.setPrintResolution(from.getPrintResolution());
+        to.setPrintSides(from.getPrintSides());
+    }
+
+    public void onActionPrint(ActionEvent event) {
+        PrinterJob printerJob = createPrinterJob();
+
+        boolean result = printerJob
+                .showPrintDialog(ta_TextArea
+                        .getScene()
+                        .getWindow());
+        if (result) {
+            Text text = new Text(ta_TextArea.getText());
+            text.setFont(ta_TextArea.getFont());
+            TextFlow textFlow = new TextFlow(text);
+            boolean printed = printerJob
+                    .printPage(textFlow);
+            if (printed) {
+                printerJob.endJob();
+            }
+        }
+    }
 
     public void getBtn_Write(ActionEvent e) {
         vb_StylePan.setVisible(false);
@@ -58,11 +152,19 @@ public class Controller {
         }
     }
 
-    public void getBtn_Finish(ActionEvent e) throws IOException //Saves file as..
+    public void getBtn_Finish(ActionEvent e)  //Saves file as..
     {
         vb_StylePan.setVisible(false);
         hb_MistakePan.setVisible(false);
         hb_FinishPan.setVisible(true);
+
+    }
+
+    public void getBtn_CreateNew(ActionEvent e) throws IOException {
+        hb_Menu.setDisable(false);
+        ta_TextArea.setDisable(false);
+
+        p_Hello.setVisible(false);
 
         Stage stage = new Stage();
         FileChooser chooser = new FileChooser();
@@ -83,8 +185,9 @@ public class Controller {
             FileWriter FW = new FileWriter(file.getAbsolutePath());
             FW.write(ta_TextArea.getText().toString());
             FW.close();
-
         }
+
+
     }
 
 //    public void autosave() throws IOException {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,26 +1,36 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Zing");
+        primaryStage.setTitle("Zing note");
         primaryStage.setScene(new Scene(root, 932, 660));
         primaryStage.show();
 
         //Call method autosave() from class Controller
 //        Controller cont = new Controller();
 //        cont.autosave();
+
+
+//        Text text = new Text(ta_TextArea.getText());
+//        Font font = Font.loadFont("file:Resources/fonts/NinaCTT.ttf", 45);
+//        text.setFont(font);
+//        text.setFill(Color.AQUAMARINE);
+
     }
 
 
